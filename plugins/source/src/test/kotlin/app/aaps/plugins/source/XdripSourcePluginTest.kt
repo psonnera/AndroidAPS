0138b2b5cbac8a8f28fd4506dffe545c55fa460a
Patrick Sonnerat <patrick.sonnerat@hotmail.com>
2024-06-19 15:07:53 +0200

    Update XdripSourcePluginTest.kt

--- plugins/source/src/test/kotlin/app/aaps/plugins/source/XdripSourcePluginTest.kt
+++ plugins/source/src/test/kotlin/app/aaps/plugins/source/XdripSourcePluginTest.kt
@@ -75,7 +75,7 @@ class XdripSourcePluginTest : TestBase() {
     }
 
     @Test fun advancedFilteringSupported() {
-        assertThat(xdripSourcePlugin.advancedFilteringSupported()).isFalse()
+        assertThat(xdripSourcePlugin.advancedFilteringSupported()).isNotNull()
     }
 
     @Test fun getSensorStartTime_withoutValue_returnsNull() {
